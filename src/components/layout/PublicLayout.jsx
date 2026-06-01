717f7827463b7455806f21a2b65b2523ddfb3d33
Will Kode <iamwillkode@gmail.com>
2026-06-01 18:05:49 +0000

    File changes

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import PublicNav from "./PublicNav";
+import Navbar from "@/components/landing/Navbar";
 import Footer from "@/components/landing/Footer";
 
 export default function PublicLayout() {
   return (
     <div className="dark min-h-screen bg-background text-foreground font-inter overflow-x-hidden">
-      <PublicNav />
+      <Navbar />
       <main className="pt-16">
         <Outlet />
       </main>
